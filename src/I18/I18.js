c5f5e8cb32d631862caaf40129c1050f557e23a9
Mady <madylabtop@gmail.com>
2025-07-22 17:04:30 +0300

    Pause Langs

--- src/I18/I18.js
+++ src/I18/I18.js
@@ -1,33 +1,33 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import LanguageDetector from 'i18next-browser-languagedetector';
-import HttpApi from 'i18next-http-backend';
+// import i18n from 'i18next';
+// import { initReactI18next } from 'react-i18next';
+// import LanguageDetector from 'i18next-browser-languagedetector';
+// import HttpApi from 'i18next-http-backend';
 
-i18n
-    .use(initReactI18next)
-    .use(LanguageDetector)
-    .use(HttpApi)
-    .init({
-        fallbackLng: 'en',
-        detection: {
-            order: [
-                "localStorage",
-                "htmlTag",
-                "cookie",
-                "sessionStorage",
-                "navigator",
-                "path",
-                "subdomain",
-            ],
-            caches: ["localStorage"],
-        },
-        debug: true,
-        interpolation: {
-            escapeValue: false, // React تقوم بالهروب تلقائيًا
-        },
-        backend: {
-            // loadPath: '/Langs/{{lng}}.json',
-        },
-    });
+// i18n
+//     .use(initReactI18next)
+//     .use(LanguageDetector)
+//     .use(HttpApi)
+//     .init({
+//         fallbackLng: 'en',
+//         detection: {
+//             order: [
+//                 "localStorage",
+//                 "htmlTag",
+//                 "cookie",
+//                 "sessionStorage",
+//                 "navigator",
+//                 "path",
+//                 "subdomain",
+//             ],
+//             caches: ["localStorage"],
+//         },
+//         debug: true,
+//         interpolation: {
+//             escapeValue: false, // React تقوم بالهروب تلقائيًا
+//         },
+//         backend: {
+//             loadPath: '/Langs/{{lng}}.json',
+//         },
+//     });
 
-export default i18n;
+// export default i18n;
--- src/componants/Select.jsx
+++ src/componants/Select.jsx
@@ -1,42 +1,42 @@
-import i18n from "i18next";
-// import { useTranslation } from "react-i18next";
-// import cookies from "js-cookie";
-import "../I18/I18"; // Ensure this imports the i18n configuration
+// import i18n from "i18next";
+// // import { useTranslation } from "react-i18next";
+// // import cookies from "js-cookie";
+// import "../I18/I18"; // Ensure this imports the i18n configuration
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-const Select = () => {
-
-
-    const lang = localStorage.getItem("i18nextLng") || "en";
-
-    // selecet Menu
-    const handleLanguageChange = (e) => {
-        const selectedLang = e.target.value;
-        i18n.changeLanguage(selectedLang);
-        localStorage.setItem("i18nextLng", selectedLang);
-    };
-
-    useEffect(() => {
-        window.document.dir = i18n.dir();
-    }, [lang]);
-
-    // const { t } = useTranslation();
+// const Select = () => {
 
 
-    return (
-        <>
-            <select
-                value={lang}
-                onChange={handleLanguageChange}
-                className="px-2 py-1 rounded cursor-pointer hover:bg-gray-800 bg-gray-800 text-white"
-                aria-label="Select language"
-            >
-                <option value="en">English</option>
-                <option value="ar">Arabic</option>
-            </select>
-        </>
-    )
-}
+//     const lang = localStorage.getItem("i18nextLng") || "en";
 
-export default Select
+//     // selecet Menu
+//     const handleLanguageChange = (e) => {
+//         const selectedLang = e.target.value;
+//         i18n.changeLanguage(selectedLang);
+//         localStorage.setItem("i18nextLng", selectedLang);
+//     };
+
+//     useEffect(() => {
+//         window.document.dir = i18n.dir();
+//     }, [lang]);
+
+//     // const { t } = useTranslation();
+
+
+//     return (
+//         <>
+//             <select
+//                 value={lang}
+//                 onChange={handleLanguageChange}
+//                 className="px-2 py-1 rounded cursor-pointer hover:bg-gray-800 bg-gray-800 text-white"
+//                 aria-label="Select language"
+//             >
+//                 <option value="en">English</option>
+//                 <option value="ar">Arabic</option>
+//             </select>
+//         </>
+//     )
+// }
+
+// export default Select
